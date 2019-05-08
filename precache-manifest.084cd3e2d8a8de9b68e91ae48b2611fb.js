1007e1b9633bde1084984947d3f3548bf7e9997d
Isha Potnis <ishapotnis30@gmail.com>
2019-05-08 14:05:18 -0400

    Hosting resume on github

---
+++ precache-manifest.084cd3e2d8a8de9b68e91ae48b2611fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bec0e8ef645163131a82b2ed90f75b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cad15876e7b298048a0b",
+    "url": "/static/css/main.84facf53.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "cad15876e7b298048a0b",
+    "url": "/static/js/main.1faf1eed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
